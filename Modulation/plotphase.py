c71e74e672de74358a88a022c7cb46a21172f0b7
Jasper Lammering <jlammering@me.com>
2018-04-18 22:58:28 +0200

    Bilder umbenannt nach auftauchen

--- Modulation/plotphase.py
+++ Modulation/plotphase.py
@@ -5,25 +5,26 @@ from scipy.optimize import curve_fit, fmin
 import uncertainties.unumpy as unp
 
 
-def plotfreq(kreisfrequenz, spannung):
+def plotphase(kreisfrequenz, spannung):
     verschiebung = 250e-9  # s
     kreisfrequenz *= 10**(6)
-    umlaufdauer = 1/kreisfrequenz
+    umlaufdauer = (2*np.pi)/kreisfrequenz
     phase = 2*np.pi*verschiebung/umlaufdauer
+    print('Umlaufdauer=', umlaufdauer)
     print('Phase= ', phase)
     x = np.cos(phase)
 
-    plt.plot(phase, spannung, 'kx', label='Messwerte')
+    plt.plot(x, spannung, 'kx', label='Messwerte')
     plt.xlabel(r'$\cos(\phi)$')
     plt.ylabel(r'$U \:/\: \si{\volt}$')
     plt.legend(loc='best')
 
     # in matplotlibrc leider (noch) nicht möglich
     plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-    plt.savefig('build/plot.pdf')
+    plt.savefig('build/plotphase.pdf')
     plt.close()
 
 
 if __name__ == '__main__':
     kreisfrequenz, spannung = np.genfromtxt('daten/gleichspannungphase.txt', unpack='True')
-    plotfreq(kreisfrequenz, spannung)
+    plotphase(kreisfrequenz, spannung)
