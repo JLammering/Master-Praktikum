06e32c2184f1cf8e3b6b7efe3950c5d3263fb473
Jasper Lammering <jlammering@me.com>
2018-11-24 21:38:24 +0100

    mein language war not gut enaf

--- Dioden-Laser/rechnungen.py
+++ Dioden-Laser/rechnungen.py
@@ -1,9 +1,9 @@
 import numpy as np
 from uncertainties import ufloat
 
-U = ufloat(3.42, 0.01)
+U = ufloat(3.42, 0.05)
 R = ufloat(100, 1)
 print("I_s = ", U/R, " A")
 
-U = ufloat(5.33, 0.01)
+U = ufloat(5.33, 0.05)
 print("I_f = ", U/R, " A")
